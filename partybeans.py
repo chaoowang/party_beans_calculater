c75a480ce1ec04b56158767dbcff64e387455466
wangchaohsuan <chaohsuan.wang@my.jcu.edu.au>
2021-04-15 14:15:20 +0800

    format output

--- partybeans.py
+++ partybeans.py
@@ -1,5 +1,6 @@
 def main():
     total = int(input("輸入本周累積消費K豆："))
+    print("---------------")
     if total < 100000:
         reward = 0
     elif total < 500000:
@@ -14,10 +15,11 @@ def main():
         reward = 750000
     else:
         reward = 1500000
-    print("可分配反豆總數：{}".format(reward))
+    print("可分配反豆總數：{}\n---------------".format(reward))
     people = []
     total = 0
     num_people = int(input("輸入要反豆人數："))
+    print("---------------")
     for i in range(num_people):
         person = int(input("輸入第{}位刷豆數量：".format(i+1)))
         people.append(person)
@@ -31,11 +33,12 @@ def main():
         reward_total += person_reward
 
     beanleft = reward-reward_total
-
     while beanleft != 0:
         for i in range(beanleft):
             person_reward_list[i] += 1
             beanleft -= 1
+
+    print("---------------\n計算結果\n：")
     for i,person_reward in enumerate(person_reward_list):
         print("第{}位反豆數：{}".format(i+1, person_reward))
 
